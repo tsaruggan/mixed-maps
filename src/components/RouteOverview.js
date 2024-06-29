0c818d11f5b516fe5c93ff6c7d02c0f7c1a3b710
tsaruggan <tsaruggan@gmail.com>
2024-06-29 15:41:08 -0400

    i'm so happy i'm the only one in this repo so i don't have to write meaningful commit messages

--- src/components/RouteOverview.js
+++ src/components/RouteOverview.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
 import styles from "@/styles/Home.module.css";
 
-const routeDirections = [
-    {
-        title: "Driving directions via ON-401 E",
-        eta: "ETA 12:54 PM",
-        duration: "18-24 min",
-        detail: "20.8 km",
-        color: "rgba(38, 70, 83, 0.25)"
-    },
-    {
-        title: "Transit directions from Kipling Station",
-        eta: "ETA 1:42 PM",
-        duration: "45 min",
-        detail: "🚶‍♂️ > ❷ > ❶",
-        color: "rgba(42, 157, 143, 0.25)"
-    },
-    {
-        title: "Biking directions via Martin Goodman Trl",
-        eta: "ETA 2:00 PM",
-        duration: "18 min",
-        detail: "4.6 km",
-        color: "rgba(233, 196, 106, 0.25)"
-    }
+// const routeDirections = [
+//     {
+//         title: "Driving directions via ON-401 E",
+//         eta: "ETA 12:54 PM",
+//         duration: "18-24 min",
+//         detail: "20.8 km",
+//         color: "rgba(38, 70, 83, 0.25)"
+//     },
+//     {
+//         title: "Transit directions from Kipling Station",
+//         eta: "ETA 1:42 PM",
+//         duration: "45 min",
+//         detail: "🚶‍♂️ > ❷ > ❶",
+//         color: "rgba(42, 157, 143, 0.25)"
+//     },
+//     {
+//         title: "Biking directions via Martin Goodman Trl",
+//         eta: "ETA 2:00 PM",
+//         duration: "18 min",
+//         detail: "4.6 km",
+//         color: "rgba(233, 196, 106, 0.25)"
+//     }
+// ]
+
+const travelModes = [
+    { name: "driving", color: "rgba(38, 70, 83, 0.25)" },
+    { name: "transit", color: "rgba(42, 157, 143, 0.25)" },
+    { name: "bicycling", color: "rgba(233, 196, 106, 0.25)" },
+    { name: "walking", color: "rgba(244, 162, 97, 0.25)" }
 ]
 
 class RouteOverview extends Component {
@@ -35,13 +42,14 @@ class RouteOverview extends Component {
                         <p>1 hr 30 min commute</p>
                     </div>
                     
-                    {routeDirections.map((routeDirection, index) => (
-                        <RouteDirectionsBlock 
-                            title={routeDirection.title}
-                            eta={routeDirection.eta}
-                            duration={routeDirection.duration}
-                            detail={routeDirection.detail}
-                            color={routeDirection.color}
+                    {this.props.directions.map((direction, index) => (
+                        <RouteDirectionsBlock
+                            key={index} 
+                            title={direction.title}
+                            eta={direction.eta.text}
+                            duration={direction.duration.text}
+                            detail={direction.distance.text}
+                            color={travelModes.find(mode => mode.name === direction.travelMode).color}
                         />
                 ))}
                 </div>
@@ -57,7 +65,7 @@ function RouteDirectionsBlock(props) {
         <div className={styles.routeDirectionsBlock} style={{backgroundColor: props.color}}>
             <div>
                 <p style={{fontWeight: '700'}}>{props.title}</p>
-                <p>{props.eta}</p>
+                <p>ETA {props.eta}</p>
             </div>
             <div style={{textAlign: 'end'}}>
                 <p style={{fontWeight: '700'}}>{props.duration}</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,26 @@ const inter = Inter({ subsets: ["latin"] });
 import RouteBuilder from "@/components/RouteBuilder";
 import RouteOverview from "@/components/RouteOverview";
 
+import { useState, useEffect } from 'react';
+
 export default function Home() {
+  const [directions, setDirections] = useState([]);
+
+  const fetchDirections = async () => {
+    try {
+      const response = await fetch('/api/directions');
+      const directions = await response.json();
+      console.log(directions);
+      setDirections(directions);
+    } catch (error) {
+      console.error('Error fetching directions:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDirections();
+  }, []);
+
   return (
     <>
       <Head>
@@ -17,7 +36,7 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.panelContainer}>
           <RouteBuilder />
-          <RouteOverview />
+          <RouteOverview directions={directions}/>
         </div>
       </main>
     </>
