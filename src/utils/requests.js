ac9158a52fc6217d8ef7010161e230ee8fa84540
tsaruggan <tsaruggan@gmail.com>
2024-07-19 15:31:30 -0400

    muahhaha

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -4,7 +4,7 @@ export async function fetchRoute(addresses, modes, dateTimeOption, dateTime) {
     const encodedAddresses = encodeURIComponent(JSON.stringify(addresses));
     const encodedModes = encodeURIComponent(JSON.stringify(modes));
 
-    const unixTimestamp = Math.floor(new Date(dateTime).getTime() / 1000); // Convert dateTime to Unix seconds
+    const unixTimestamp = Math.floor(new Date(dateTime).getTime() / 1000); // Convert dateTime to Unix seconds.
     let URL = `/api/directions?addresses=${encodedAddresses}&modes=${encodedModes}`;
     if (dateTimeOption == "Depart at") {
         URL += `&departAt=${unixTimestamp}`;
